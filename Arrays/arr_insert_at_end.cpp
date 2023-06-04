80b8ccbcb71ca85b2bab04924c70b4684dc7bb50
Vishal Pange <51144803+UnfazedHope@users.noreply.github.com>
2023-06-04 12:01:04 -0600

    Arr insert at End

--- Arrays/arr_insert_at_end.cpp
+++ Arrays/arr_insert_at_end.cpp
@@ -1,6 +1,10 @@
+// You only need to insert the given element at 
+// the end, i.e., at index sizeOfArray - 1. You may 
+// assume that the array already has sizeOfArray - 1
+// elements. 
 void insertAtEnd(int arr[],int sizeOfArray,int element)
 {
     //Your code here
-    // Just append the element at end of an array
+    
     arr[sizeOfArray-1] = element;
 }
