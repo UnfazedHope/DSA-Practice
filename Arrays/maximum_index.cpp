77496b9eb9df82afed37cc68ed54fe94fef18e33
Vishal Pange <51144803+UnfazedHope@users.noreply.github.com>
2023-06-06 10:32:07 -0600

    Maximum index

--- Arrays/maximum_index.cpp
+++ Arrays/maximum_index.cpp
@@ -26,4 +26,46 @@ class Solution{
 
 // Optimized Solution :-
 
-// Working on it!
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int A[], int N) 
+    { 
+        // Your code here
+        
+        vector<int>v;
+        
+        v.push_back(0);
+
+        int maxdiff=INT_MIN;
+
+        for(int i=1;i<N;i++)
+        {
+            int n=v.size();
+            
+            if(A[v[n-1]]>A[i]){
+                v.push_back(i);
+            }
+            else{
+                for(int j=n-1;j>=0;j--){
+                 
+                    if(A[v[j]]<=A[i]){
+                        maxdiff=max(maxdiff,(i-v[j]));
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(maxdiff==INT_MIN)
+            return 0;
+
+        return maxdiff;
+        
+    }
+};
