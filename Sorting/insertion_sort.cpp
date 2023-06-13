67365b801b3cd11deeb1212a8be391f6fb911995
Vishal Pange <51144803+UnfazedHope@users.noreply.github.com>
2023-06-13 12:32:38 -0600

    Insertion sort

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -1,4 +1,27 @@
+1st method :-
+ 
  public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            int temp=0;
+            
+            while (j > 0 && arr[j-1] > arr[j]) {
+                temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j = j - 1;
+            }
+        }
+    }
+
+2nd method :- 
+
+public:
     //Function to sort the array using insertion sort algorithm.
     void insertionSort(int arr[], int n)
     {
